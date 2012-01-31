d063e96963be169fa08c864ae34f0e28048b4ee6
Charles Plessy <plessy@riken.jp>
2012-01-31 21:50:46 +0900

    Rough implementation with codon IDs.  Needs triplechecking.
    V and J segment names need to be corrected.

--- R/yassai.nomenclature.R
+++ R/yassai.nomenclature.R
@@ -9,46 +9,50 @@ tocodons <- function (dna)
 	unlist(strsplit(dna, "(?<=...)", perl=TRUE))
 
 # Split the reference V segment and the CDR3 nucleotidic sequence in codons.
-V <- tocodons(V_after_C[as.character(clonotype[[2]]),]) # do sanity checks !
+V_name <- as.character(clonotype[[2]])
+V <- tocodons(V_after_C[V_name,]) # do sanity checks !
 dna <- tocodons(clonotype[[5]])
 
 # Split the reversed reference J segment and CDR3 in codons.
-J <- tocodons(strReverse(J_before_FGxG[as.character(clonotype[[3]]),])) # do sanity checks !
+J_name <- as.character(clonotype[[3]])
+J <- tocodons(strReverse(J_before_FGxG[J_name,])) # do sanity checks !
 dnarev <- tocodons(strReverse(clonotype[[5]]))
 
 pep <- clonotype[[7]]
 
 # True if the reference and CDR3 codons are identical.
-is.somatic <- function (V, dna) {
+is.germline <- function (V, dna) {
 	V <- substr(V,1,3)
 	dna <- substr(dna,1,3)
 	answer <- tolower(V) == tolower(dna)
-	answer[is.na(answer)] <- FALSE  # Replace NA per FALSE; is.somatic is used in a while loop.
+	answer[is.na(answer)] <- FALSE  # Replace NA per FALSE; is.germline is used in a while loop.
 	return(answer)
 }
 
-# Counts how many consecutive codons are somatically encoded from ends to center.
-V_somatic <- 0
-while( is.somatic(V[V_somatic + 1],dna[V_somatic + 1]) )
-	V_somatic <- V_somatic + 1
+# Counts how many consecutive codons are germlineally encoded from ends to center.
+V_germline <- 0
+while( is.germline(V[V_germline + 1],dna[V_germline + 1]) )
+	V_germline <- V_germline + 1
 
-J_somatic <- 0
-while( is.somatic(J[J_somatic + 1],dnarev[J_somatic + 1]) )
-        J_somatic <- J_somatic + 1
+J_germline <- 0
+while( is.germline(J[J_germline + 1],dnarev[J_germline + 1]) )
+        J_germline <- J_germline + 1
 
-# Convert to lower case and remove all but the most central somatically encoded codons.
-pep <- toupper(pep) # Just in case
-substr(pep,1,V_somatic) <- tolower(substr(pep,1,V_somatic))
-pep <- substring(pep,V_somatic)
+# Convert to lower case and remove all but the most central germlineally encoded codons.
+CDR3aa <- toupper(pep) # Just in case
+substr(CDR3aa,1,V_germline) <- tolower(substr(CDR3aa,1,V_germline))
+CDR3aa <- substring(CDR3aa,V_germline)
 
-substring(pep, nchar(pep) - J_somatic, nchar(pep)) <- tolower(substring(pep, nchar(pep) - J_somatic, nchar(pep)))
-pep <- substring(pep, 1, nchar(pep) - J_somatic)
+substring(CDR3aa, nchar(CDR3aa) - J_germline, nchar(CDR3aa)) <- tolower(substring(CDR3aa, nchar(CDR3aa) - J_germline, nchar(CDR3aa)))
+CDR3aa <- substring(CDR3aa, 1, nchar(CDR3aa) - J_germline)
 
 # Convert the TRAV and TRAJ names
-
+# To Do !
 
 # Determine the ID for the remaining codons.
+IDs <- paste(codon_ids[dna[(V_germline +1) : (nchar(pep) - J_germline -1)],"id"], collapse="")
+if ( (V_germline +1) > (nchar(pep) - J_germline -1) ) IDs <- ""
 
 # Construct and return the CDR3 in Yassai et al's nomenclature.
-return( pep )
+return( paste(CDR3aa, ".", IDs, V_name, J_name, "L", nchar(pep), sep=""))
 }
