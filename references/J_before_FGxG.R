21ae3c1ccc3f0f51e199a38aea7ea664858d58a8
Charles Plessy <plessy@riken.jp>
2012-08-09 13:19:41 +0900

    Segment data for R, and mechanism to refresh it.  This needs Bioconducor's Biostrings package.

--- references/J_before_FGxG.R
+++ references/J_before_FGxG.R
@@ -1,13 +1,11 @@
-J_before_FGxG <- function (...) {
-        # Load data/J_before_FGxG, depend on Biostrings
-	if ( file.exists (system.file("data/J_before_FGxG.fa", package = "clonotypeR") ) ) {
-	  file_to_load <- system.file("data/J_before_FGxG.fa", package = "clonotypeR" ) }
-	if ( file.exists("data/J_before_FGxG.fa") ) {
-	  file_to_load <- "data/J_before_FGxG.fa" }
-	if ( file.exists("J_before_FGxG.fa") ) {
-	  file_to_load <- "J_before_FGxG.fa" }
-	if ( ! exists("file_to_load")) {
-	  stop('Could not find the data file "J_before_FGxG.fa"') }
-	data.frame(sequence=as.character(Biostrings:::read.DNAStringSet(file_to_load)), stringsAsFactors=FALSE)
-}
-J_before_FGxG <- J_before_FGxG()
+library(Biostrings)
+
+file_to_load <- "J_before_FGxG.fa"
+
+x <- read.DNAStringSet(file_to_load)
+
+x <- as.character(x)
+
+x <- data.frame(sequence=x, stringsAsFactors=FALSE)
+
+write.table(x, file="../data/J_before_FGxG.txt")
--- references/V_after_C.R
+++ references/V_after_C.R
@@ -1,14 +1,11 @@
-V_after_C <- function (...) {
-        # Load references/V_after_C.fa, depend on Biostrings
-	if ( file.exists (system.file("data/V_after_C.fa", package = "clonotypeR") ) ) {
-	  file_to_load <- system.file("data/V_after_C.fa", package = "clonotypeR" ) }
-	if ( file.exists("data/V_after_C.fa") ) {
-	  file_to_load <- "data/V_after_C.fa" }
-	if ( file.exists("V_after_C.fa") ) {
-	  file_to_load <- "V_after_C.fa" }
-	if ( ! exists("file_to_load")) {
-	  stop('Could not find the data file "V_after_C.fa"') }
-	data.frame(sequence=as.character(Biostrings:::read.DNAStringSet(file_to_load)), stringsAsFactors=FALSE)
-}
-V_after_C <- V_after_C()
+library(Biostrings)
 
+file_to_load <- "V_after_C.fa"
+
+x <- read.DNAStringSet(file_to_load)
+
+x <- as.character(x)
+
+x <- data.frame(sequence=x, stringsAsFactors=FALSE)
+
+write.table(x, file="../data/V_after_C.txt")
