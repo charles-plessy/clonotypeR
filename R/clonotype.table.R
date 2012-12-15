436a2e1042b9e9ca73abd1fe7d7c24e0ebaf413d
Charles Plessy <plessy@riken.jp>
2012-12-15 17:23:36 +0100

    Corrected syntax error in a stop() command.

--- R/clonotype.table.R
+++ R/clonotype.table.R
@@ -13,7 +13,7 @@ if ( ! is.data.frame(from) )
 	stop ("Input clonotypes must be in a data frame.")
 
 if ( ! is.logical(from$unproductive) )
-	stop ("Input missing \dQuote{unproductive} column.")
+	stop ('Input missing "unproductive" column.')
 
 # The following function counts, for a single library, the occurrences of
 # segments, CDR3s or combinations of them, and return them as a simple data
