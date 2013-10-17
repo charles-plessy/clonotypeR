f7a94d4a6bf78f6a6516c39923b45fa595e6bc30
c.plessy <c.plessy@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-10-17 09:58:59 +0000

    2013-10-17  Charles Plessy <plessy@riken.jp>         * Commited 1.1.1 to Bioconductor.         * common_clonotypes: new mode to calculate the "abundance" relatively           to one library.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/clonotypeR@81757 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/common_clonotypes.R
+++ R/common_clonotypes.R
@@ -1,58 +1,96 @@
-common_clonotypes <- function (group1, group2, mode="groups", data) {
+setGeneric(
+    "common_clonotypes",
+    function (group1, group2, mode, data)
+        standardGeneric("common_clonotypes")
+)
 
-# Test if arguments are correct.
+## When the input is a single list of libraries, return the list of common clonotypes.
 
-if ( ! (is.data.frame(data) | is.matrix(data)) )
-	stop ( paste (dQuote("data"), "argument must be a data frame or a matrix of clonotypes."))
+setMethod(
+    common_clonotypes,
+    c(group1="character", group2="missing", mode="missing", data="data.frame"),
+    function(group1, data) {
+       
+    if ( FALSE %in% ( group1 %in% colnames(data) ) )
+        stop ("Unknown library.")
 
-if ( ! (mode == "groups" | mode == "matrix") )
-	stop (paste (sep='', dQuote("mode"), " argument must be ", dQuote("groups"), " or ", dQuote("matrix"), "."))
+    if ( length(group1) == 1) (
+        selector <- data[,group1] > 0
+    ) else (
+        selector <- rowSums( data[, group1] ) > 0
+    )       
+    return (rownames(data)[selector])
+})
 
-if (   missing("group1") & missing("group2") )
-	mode <- "matrix"
+## To Do: common_clonotypes(group1="foo", group2="bar") should return the same as common_clonotypes(group1=c("foo", "bar"))
 
-if ( ! missing("group1") && ! is.character(group1) )
-	stop ( paste ( dQuote("group1"), "argument must be a character vector of library names."))
+## When the input is two lists of libraries, return the list of common
+## clonotypes between the two groups, after pooling the libraries in each group.
 
-if ( ! missing("group2") && ! is.character(group2) )
-	stop ( paste ( dQuote("group2"), "argument must be a character vector of library names."))
+setMethod(
+    common_clonotypes,
+    c(group1="character", group2="character", mode="missing", data="data.frame"),
+    function(group1, group2, data) {
 
-found <- function (group) {
-	if ( FALSE %in% ( group %in% colnames(data) ) )
-		stop ("Unknown library.")
+    if ( FALSE %in% ( c(group1, group2) %in% colnames(data) ) )
+        stop ("Unknown library.")
 
-	if ( length(group) == 1) (
-		selector <- data[,group] > 0
-	) else (
-		selector <- rowSums( data[, group] ) > 0
-	)
-}
+    intersect(
+        common_clonotypes(group1=group1, data=data),
+        common_clonotypes(group1=group2, data=data)
+    )
+})    
 
-# Return row names in groups mode.
+## When the input is a data frame, return a matrix of counts.
 
-if ( mode == "groups" ) {
-	if ( missing("group2") ) {
-		selector <- found(group1)
-	} else {
-		selector <- found(group1) & found(group2)
-	}
-	return ( rownames(data)[selector])
-}
+setMethod(
+    common_clonotypes,
+    c(group1="missing", group2="missing", mode="ANY", data="data.frame"),
+    function(mode="count", data) {
 
-# Return a matrix of pairwise number of common clonotypes in matrix mode.
+    if(missing(mode))
+        mode <- "count"
 
-if ( mode == "matrix") {
+    if ( ! (mode == "count" | mode == "abundance") )
+        stop (paste (sep='', dQuote("mode"), " argument must be ", dQuote("count"), " or ", dQuote("abundance"), "."))
 
-	m <- matrix(nrow=length(colnames(data)), ncol=length(colnames(data)))
+    numberOfLibs = dim(data)[2]
 
-	for (i in 1:length(colnames(data)) ) {
-		for (j in 1:length(colnames(data)) ) {
-			m[i,j] <- length(common_clonotypes(group1=colnames(data)[i], group2=colnames(data)[j], mode="groups", data=data))
-		}
-	}
+    # Prepare the matrix to hold the data
+    m <- matrix(nrow=numberOfLibs, ncol=numberOfLibs)
 	colnames(m) <- colnames(data)
 	rownames(m) <- colnames(data)
-	return (m)
-}
 
-}
+    # Fill the matrix
+    for (i in 1:numberOfLibs) {
+        for (j in i:numberOfLibs) {
+            commonIJ <- common_clonotypes(
+                            group1=colnames(data)[i],
+                            group2=colnames(data)[j],
+                            data=data)
+            if (mode == "count") {
+                m[i,j] <- length(commonIJ)
+                m[j,i] <- m[i,j]
+            }
+            if (mode == "abundance") {
+                m[i,j] <- sum(data[commonIJ,i])
+                m[j,i] <- sum(data[commonIJ,j])
+            }
+        }
+    }
+
+    return (m)
+})
+
+## When the input is a matrix, convert it to a data frame.
+
+setMethod(
+    common_clonotypes,
+    c(group1="missing", group2="missing", mode="ANY", data="matrix"),
+    function(mode, data) {
+
+    if(missing(mode))
+        mode <- "count"
+
+    common_clonotypes(mode=mode, data=as.data.frame(data))
+})
